fda3d9738ac2b54522387427b6d6971a930744df
Krishna Murthy <krishna.murthy@grabtaxi.com>
2019-04-17 14:23:02 +0530

    change

--- slices_2.go
+++ slices_2.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func get() []byte {  
     raw := make([]byte,10000)
-    fmt.Println(len(raw),cap(raw),&raw[0]) //prints: 10000 10000 <byte_addr_x>
+    fmt.Println(len(raw),cap(raw),&raw[0])
     res := make([]byte,3)
     copy(res,raw[:3])
     return res
@@ -12,5 +12,5 @@ func get() []byte {
 
 func main() {  
     data := get()
-    fmt.Println(len(data),cap(data),&data[0]) //prints: 3 3 <byte_addr_y>
+    fmt.Println(len(data),cap(data),&data[0])
 }
