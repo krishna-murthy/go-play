372397570959c9d1531eff39a2668b15007cf514
Krishna Murthy <krishna.murthy@grabtaxi.com>
2019-04-17 11:14:11 +0530

    adding more tests

---
+++ nil_call.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type gopher struct {
+  name string
+}
+
+func (g *gopher) Name() {
+  fmt.Println("My name is gopher.")
+}
+
+func main() {
+  // Thought the pointer is nil, its valid to call the method on it
+  var g *gopher
+  fmt.Printf("content: %+v %+p\n", g, g)
+  g.Name()
+}
